3799aff73005fd74f03254f078df24ed313df8e8
robin647 <mhrobin471@gmail.com>
2026-09-16 09:17:59 +0600

    make production ready

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,13 +1,25 @@
+
 from .base import *  # noqa
 
 DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS",
+    default=[
+        "stock-and-blance-management.onrender.com",
+        "localhost",
+        "127.0.0.1",
+    ],
+)
 
 # --- Security hardening for production ---
 SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT", default=True)
+
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
 SECURE_HSTS_SECONDS = 60 * 60 * 24 * 7  # 7 days
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
+
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
